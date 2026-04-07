7d7eba8b5c750be746b196e99d1ddc6c4b9b926e
F Melgoza <fmelgoza@Mac.lan>
2026-04-07 10:23:59 -0500

    Use npx for Zowe CLI

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,15 +2,15 @@
 
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+if ! npx zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+  npx zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
 else
   echo "Directory already exists."
 fi
 
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive \
-  --binary-files "cobol-check-0.2.9.jar"
+npx zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive \
+  --binary-files "cobol-check-0.2.19.jar"
 
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+npx zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
